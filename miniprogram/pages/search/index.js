cf0ce595e02ce2fe412fbc57d9984704250ab938
Lanceloia <3509175458@qq.com>
2019-03-12 18:51:48 +0800

    Add an addition check at search

--- miniprogram/pages/search/index.js
+++ miniprogram/pages/search/index.js
@@ -8,46 +8,55 @@ Page({
 
   },
   formSubmit: function (e) {
-    console.log('form发生了submit.search事件，携带数据为：', e.detail.value)
-    const db = wx.cloud.database()
-    const posts = db.collection('posts')
-    const _ = db.command
-    var self = this
-    posts.where(_.and(
-      {
-        cardid: _.eq(e.detail.value.cardid),
-        cardname: _.eq(e.detail.value.cardname)
-      },
-      {
-        cardid: _.neq(''),
-        cardname: _.neq('')
-      }
-    )).get({
-      success: function(res){
-        console.log(res)
-        if(res.data.length){
-          console.log("query success")
-          wx.navigateTo({
-            url: '/pages/post/post?postid=' + res.data[0]._id
-          })
+    if (!(e.detail.value.cardid == "" || e.detail.value.cardname == "")) {
+      console.log('form发生了submit.search事件，携带数据为：', e.detail.value)
+      const db = wx.cloud.database()
+      const posts = db.collection('posts')
+      const _ = db.command
+      var self = this
+      posts.where(_.and(
+        {
+          cardid: _.eq(e.detail.value.cardid),
+          cardname: _.eq(e.detail.value.cardname)
+        },
+        {
+          cardid: _.neq(''),
+          cardname: _.neq('')
         }
-        else {
-          console.log("failed to get")
-          wx.showModal({
-            content: '没有找到失物信息。',
-            showCancel: false,
-            success: function (res) {
-              if (res.confirm) {
-                console.log('用户点击确定')
+      )).get({
+        success: function (res) {
+          console.log(res)
+          if (res.data.length) {
+            console.log("query success")
+            wx.navigateTo({
+              url: '/pages/post/post?postid=' + res.data[0]._id
+            })
+          }
+          else {
+            console.log("failed to get")
+            wx.showModal({
+              content: '没有找到失物信息。',
+              showCancel: false,
+              success: function (res) {
+                if (res.confirm) {
+                  console.log('用户点击确定')
+                }
               }
-            }
-          })
+            })
+          }
         }
-      }
-    })
+      })
+    }
+    else{
+      console.log("exists empty item")
+      wx.showToast({
+        title: '表单不能为空',
+        icon: 'loading',
+      })
+    }
   },
 
-  formReset: function (){
+  formReset: function () {
     console.log("查询表单发出reset动作")
   },
   /**
