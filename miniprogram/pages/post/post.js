2a42c2d85549e77f354bdcad7aefc4f1858304ed
RicoloveFeng <kakagou.mc@outlook.com>
2019-03-12 15:03:05 +0800

    post

---
+++ miniprogram/pages/post/post.js
@@ -0,0 +1,93 @@
+// pages/post/post.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    postname:"标题读取中",
+    imgsrc:"card.png",
+    cardid: "ID读取中",
+    cardname: "主人读取中",
+    location: "位置读取中",
+    contact: "联系方式读取中",
+    msg:"备注信息读取中"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    if(options.postid == -1){
+      console.log('展示最新的记录')
+      const db = wx.cloud.database()
+      const posts = db.collection('posts')
+      const _ = db.command
+      var self = this
+      posts.limit(2)
+        .get({
+        success(res) {
+          console.log(res.data[0].cardid)
+          self.setData({
+            postname: "校园卡",
+            cardid: res.data[0].cardid,
+            cardname: res.data[0].cardname,
+            location: res.data[0].location,
+            contact: res.data[0].contact,
+            msg: res.data[0].msg
+          })
+        }
+      })
+    }
+ 
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
