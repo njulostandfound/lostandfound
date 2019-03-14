5e7641c17d39b199d65dcb9192f2a42d8c637ba3
RicoloveFeng <kakagou.mc@outlook.com>
2019-03-14 15:31:35 +0800

    stream

---
+++ cloudfunctions/stream/index.js
@@ -0,0 +1,23 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  console.log("进入云函数stream")
+  console.log(event)
+  const db = cloud.database()
+  const posts = db.collection('posts')
+  try {
+    if(event.searchKeyword == "all"){
+      return await posts.orderBy("date", "desc").limit(event.rescount).get()
+    }
+    else{
+      return await posts.where({title: event.searchKeyword}).orderBy("date", "desc").limit(event.rescount).get()
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
--- miniprogram/pages/stream/stream.js
+++ miniprogram/pages/stream/stream.js
@@ -1,18 +1,47 @@
 // pages/stream/stream.js
+//credit: http://www.cnblogs.com/thebeauty/p/6599612.html
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    searchKeyword: '',
+    searchPostList: [],
+    isFromSearch: true,
+    searchPageNum: 20, 
+    rescount: 15,
+    searchLoading: false,
+    searchLoadingComplete: false
   },
 
+  fetchSearchList: function () {
+    console.log("get data")
+    var self = this
+    wx.cloud.callFunction({
+      name:"stream",
+      data:{
+        searchKeyword: self.data.searchKeyword,
+        rescount: self.data.rescount
+      },
+      success(res){
+        console.log(res)
+        self.setData({
+          searchPostList: res.result.data
+        })
+      }
+    })
+  },
+
+  searchScrollLower: function () {
+
+   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({searchKeyword: options.key})
+    this.fetchSearchList()
   },
 
   /**
