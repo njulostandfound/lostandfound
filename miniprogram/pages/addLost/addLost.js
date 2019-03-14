8bcf9224b6cd8e9327d497fdabadccbb03662881
Lanceloia <3509175458@qq.com>
2019-03-14 19:08:30 +0800

    Lanceloa 2019/3/14 19:08

--- miniprogram/pages/addLost/addLost.js
+++ miniprogram/pages/addLost/addLost.js
@@ -56,6 +56,10 @@ Page({
       })
     }
   }, 
+  formSubmit2: function (e) {
+    console.log('hhh')
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  }, 
     /**
    * 生命周期函数--监听页面加载
    */
